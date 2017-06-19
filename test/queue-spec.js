0ecc41f17a26efc6709ceed2d11c0b4450a43ceb
Ryan Workman <ryan.workman@dish.com>
2017-06-19 19:24:23 +0000

    Add tail and count functionality to queue

--- test/queue-spec.js
+++ test/queue-spec.js
@@ -12,4 +12,31 @@ describe('Queue handling data', function() {
 
     expect(q.queue).to.deep.equal([1]);
   });
+
+  it('can have more than one data item pushed', function() {
+    var q = this.q;
+    q.push(1);
+    q.push(0);
+    q.push(1);
+
+    expect(q.queue).to.deep.equal([1,0,1]);
+  });
+
+  it('knows how many pieces of data are stored', function() {
+    var q = this.q;
+    q.push(1);
+    q.push(0);
+    q.push(1);
+
+    expect(q.count()).to.equal(3);
+  });
+
+  it('displays the tail end of data', function() {
+    var q = this.q;
+    q.push(1);
+    q.push(0);
+
+    expect(q.tail()).to.equal(0);
+  });
+
 });
