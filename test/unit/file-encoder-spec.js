104bea3d401c6a4bc5c805ab695af3e4a4abc76f
Ryan Workman <ryan.workman@dish.com>
2017-06-20 20:20:14 +0000

    Add file reading and encoding functionality

--- test/unit/file-encoder-spec.js
+++ test/unit/file-encoder-spec.js
@@ -1,9 +1,11 @@
 var expect = require('../test-helper').expect;
 var FileEncoder = require('../test-helper').fileEncoder;
+var fs = require('fs');
 
 describe('File Encoder', function() {
   beforeEach(function() {
     this.fe = new FileEncoder;
+    this.encodePath = '../../bin/.encoded/encoded-test.txt';
   });
 
   it('loads text files', function() {
@@ -11,5 +13,10 @@ describe('File Encoder', function() {
 
     expect(fe.loadFile('testing.txt')).to.equal(true);
     expect(fe.fileContents()).to.equal('This is a test text file. Hello world... foo bar... and all the cliche coding terms.\nSudo apt-get all the things.');
+  });
+
+  it('encodes loaded text file to bin/.encoded', function() {
+    var fe = this.fe;
+    fe.encode('testing.txt', 'encoded-test.txt');
   });
 });
